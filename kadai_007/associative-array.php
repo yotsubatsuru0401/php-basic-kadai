523c752ecfefaf0d86a6b5c358deed34f0b9a091
yotsubatsuru0401 <168731026+yotsubatsuru0401@users.noreply.github.com>
2024-08-14 08:21:17 +0900

    課題提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+ </head>
+ 
+ <body>
+     <p>
+         <?php
+         
+         $data = ['name' => 'onion', 'price' => 200, 'weight' => '160'];
+ 
+         // 連想配列の値を出力する
+         print_r($data);
+         ?>
+    </p>
+</body>
+</html>
